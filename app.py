cd90bfeeaf037badf52f89da0b9b31b20b7fe4c1
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-08-23 07:42:43 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+print("""
+
+░█▀▀█ █▀▀ █── █── 　 ▀▀█▀▀ █▀▀ █▀▀ █──█ 
+░█─── █▀▀ █── █── 　 ─░█── █▀▀ █── █▀▀█ 
+░█▄▄█ ▀▀▀ ▀▀▀ ▀▀▀ 　 ─░█── ▀▀▀ ▀▀▀ ▀──▀
+""")
+
+print('1. Cadastrar Celular')
+print('2. Listar Celulares')
+print('3. Ativar Celulares')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha um opção:')
+print('Você escolheu a opção:', opcao_escolhida)
